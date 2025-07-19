f84cf4951eaa56149abeb8d01126dbf47c926274
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2025-07-19 14:09:06 +0530

    header push

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-muted/50 py-12">
+                    <div className="container mx-auto px-4 text-center text-gray-200 ">
+                      <p>Made by Biswajit Sarkar</p>
+                    </div>
+                  </footer>
+    </div>
+  )
+}
+
+export default Footer
