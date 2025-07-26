c1671e9c665f7d059a79438ee9e4398dea8d4aa2
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2025-07-26 15:00:11 +0530

    databse push

---
+++ prisma/migrations/20250726075659_create_tables/migration.sql
@@ -0,0 +1,113 @@
+-- CreateEnum
+CREATE TYPE "DemandLevel" AS ENUM ('HIGH', 'MEDIUM', 'LOW');
+
+-- CreateEnum
+CREATE TYPE "MarketOutlook" AS ENUM ('POSITIVE', 'NETURAL', 'NEGATIVE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkUserId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "imageUrl" TEXT,
+    "industry" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "bio" TEXT,
+    "experience" INTEGER,
+    "skills" TEXT[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Assesments" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "quizScore" DOUBLE PRECISION NOT NULL,
+    "questions" JSONB[],
+    "catagory" TEXT NOT NULL,
+    "improvements" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Assesments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Resume" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Resume_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CoverLetter" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "jobDescription" TEXT,
+    "compnayName" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CoverLetter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "IndustryInsight" (
+    "id" TEXT NOT NULL,
+    "industry" TEXT NOT NULL,
+    "salaryRange" JSONB[],
+    "growthRate" DOUBLE PRECISION NOT NULL,
+    "demandLevel" "DemandLevel" NOT NULL,
+    "topSkills" TEXT[],
+    "marketOutLook" "MarketOutlook" NOT NULL,
+    "keyTrends" TEXT[],
+    "recomendedSkills" TEXT[],
+    "lastUpdated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "nextUpdate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "IndustryInsight_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkUserId_key" ON "User"("clerkUserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Assesments_userId_idx" ON "Assesments"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Resume_userId_key" ON "Resume"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CoverLetter_userId_key" ON "CoverLetter"("userId");
+
+-- CreateIndex
+CREATE INDEX "CoverLetter_userId_idx" ON "CoverLetter"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "IndustryInsight_industry_key" ON "IndustryInsight"("industry");
+
+-- CreateIndex
+CREATE INDEX "IndustryInsight_industry_idx" ON "IndustryInsight"("industry");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_industry_fkey" FOREIGN KEY ("industry") REFERENCES "IndustryInsight"("industry") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Assesments" ADD CONSTRAINT "Assesments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Resume" ADD CONSTRAINT "Resume_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CoverLetter" ADD CONSTRAINT "CoverLetter_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
